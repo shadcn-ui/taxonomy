eccfb1b027ef73a314869cd5667bd6f32e6fbf72
shadcn <arshad@chummun.com>
2022-11-10 19:16:04 +0400

    fix: update github stars

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link"
-import { toWords } from "number-to-words"
 
 import { Icons } from "@/components/icons"
 
 async function getGitHubStars(): Promise<string | null> {
   const response = await fetch("https://api.github.com/repos/shadcn/taxonomy", {
+    headers: {
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+    },
     next: {
       revalidate: 60,
     },
@@ -16,7 +19,7 @@ async function getGitHubStars(): Promise<string | null> {
 
   const json = await response.json()
 
-  return toWords(json["stargazers_count"])
+  return parseInt(json["stargazers_count"]).toLocaleString()
 }
 
 export default async function IndexPage() {
@@ -193,9 +196,29 @@ export default async function IndexPage() {
             </Link>
           </p>
         </div>
-        <h3 className="inline-flex items-center gap-2 text-lg font-medium capitalize md:text-xl">
-          <span>{stars} stars on GitHub</span>
-        </h3>
+        <Link
+          href="https://github.com/shadcn/taxonomy"
+          target="_blank"
+          rel="noreferrer"
+          className="flex"
+        >
+          <div className="flex h-10 w-10 items-center justify-center space-x-2 rounded-md border border-slate-600 bg-slate-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              className="h-5 w-5 text-white"
+            >
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
+            </svg>
+          </div>
+          <div className="flex items-center">
+            <div className="h-4 w-4 border-y-8 border-r-8 border-l-0 border-solid border-y-transparent border-r-slate-800"></div>
+            <div className="flex h-10 items-center rounded-md border border-slate-800 bg-slate-800 px-4 font-medium text-slate-200">
+              {stars} stars on GitHub
+            </div>
+          </div>
+        </Link>
       </section>
       <div className="md:py-18 container py-12 lg:py-24">
         <hr className="border-slate-100" />
