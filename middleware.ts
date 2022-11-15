8f13cb81952738d3f14ae41452739444c70e6916
Andrew Ellis <127468+ellisio@users.noreply.github.com>
2022-11-15 00:12:01 -0700

    feat: add from redirect for auth (#18)
    * feat: add from redirect for auth
    
    * fix: add missing import

--- middleware.ts
+++ middleware.ts
@@ -19,7 +19,14 @@ export default withAuth(
     }
 
     if (!isAuth) {
-      return NextResponse.redirect(new URL("/login", req.url))
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+
+      return NextResponse.redirect(
+        new URL(`/login?from=${encodeURIComponent(from)}`, req.url)
+      );
     }
   },
   {
