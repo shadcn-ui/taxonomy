f74160fc0f62ea61a2d6f53dce2a6bd30f63700f
shadcn <arshad@chummun.com>
2022-11-21 20:59:56 +0400

    fix: add priority to hero image

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -37,7 +37,7 @@ export default async function IndexPage() {
   return (
     <>
       <section className="container grid items-center justify-center gap-6 pt-6 pb-8 md:pt-10 md:pb-12 lg:pt-16 lg:pb-24">
-        <Image src={hero} width={250} alt="Hero image" />
+        <Image src={hero} width={250} alt="Hero image" priority />
         <div className="mx-auto flex flex-col items-start gap-4 lg:w-[52rem]">
           <h1 className="text-3xl font-bold leading-[1.1] tracking-tighter sm:text-5xl md:text-6xl">
             What&apos;s going on here?
@@ -187,9 +187,9 @@ export default async function IndexPage() {
             >
               GitHub
             </Link>
-            . I&apos;m also documenting everything{" "}
+            .
             <Link href="/docs" className="underline underline-offset-4">
-              here
+              I&apos;m also documenting everything here
             </Link>
             .
           </p>
