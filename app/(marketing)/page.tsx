4960b67b4ae7764fe02621e45ef7b96fccd04565
Sharikh Ahmed <sharikhahmed30@gmail.com>
2022-11-25 22:55:46 +0530

    [FEAT] remove code which is not required

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -54,14 +54,6 @@ export default async function IndexPage() {
           >
             Get Started
           </Link>
-          <Link
-            href={siteConfig.links.github}
-            target="_blank"
-            rel="noreferrer"
-            className="relative inline-flex h-11 items-center rounded-md border border-slate-200 bg-white px-8 py-2 font-medium text-slate-900 transition-colors hover:bg-slate-50 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
-          >
-            GitHub
-          </Link>
         </div>
       </section>
       <hr className="border-slate-200" />
@@ -163,63 +155,8 @@ export default async function IndexPage() {
             </div>
           </div>
         </div>
-        <div className="mx-auto flex flex-col gap-4 md:max-w-[52rem]">
-          <p className="max-w-[85%] leading-normal text-slate-700 sm:text-lg sm:leading-7">
-            Taxonomy also includes a blog and a full-featured documentation site
-            built using Contentlayer and MDX.
-          </p>
-        </div>
       </section>
       <hr className="border-slate-200" />
-      <section className="container grid justify-center gap-6 py-8 md:py-12 lg:py-24">
-        <div className="mx-auto flex flex-col gap-4 md:max-w-[52rem]">
-          <h2 className="text-3xl font-bold leading-[1.1] tracking-tighter sm:text-3xl md:text-6xl">
-            Proudly Open Source
-          </h2>
-          <p className="max-w-[85%] leading-normal text-slate-700 sm:text-lg sm:leading-7">
-            Taxonomy is open source and powered by open source software. The
-            code is available on{" "}
-            <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-              className="underline underline-offset-4"
-            >
-              GitHub
-            </Link>
-            .
-            <Link href="/docs" className="underline underline-offset-4">
-              I&apos;m also documenting everything here
-            </Link>
-            .
-          </p>
-        </div>
-        {stars && (
-          <Link
-            href={siteConfig.links.github}
-            target="_blank"
-            rel="noreferrer"
-            className="flex"
-          >
-            <div className="flex h-10 w-10 items-center justify-center space-x-2 rounded-md border border-slate-600 bg-slate-800">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                className="h-5 w-5 text-white"
-              >
-                <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"></path>
-              </svg>
-            </div>
-            <div className="flex items-center">
-              <div className="h-4 w-4 border-y-8 border-r-8 border-l-0 border-solid border-y-transparent border-r-slate-800"></div>
-              <div className="flex h-10 items-center rounded-md border border-slate-800 bg-slate-800 px-4  font-medium text-slate-200">
-                {stars} stars on GitHub
-              </div>
-            </div>
-          </Link>
-        )}
-      </section>
     </>
   )
 }
--- config/marketing.ts
+++ config/marketing.ts
@@ -3,26 +3,12 @@ import { MarketingConfig } from "types"
 export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
-      title: "Features",
-      href: "/features",
-      disabled: true,
-    },
-    {
       title: "Pricing",
       href: "/pricing",
     },
     {
       title: "Blog",
-      href: "/blog",
-    },
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
-    {
-      title: "Contact",
-      href: "/contact",
-      disabled: true,
+      href: "/blog"
     },
   ],
 }
