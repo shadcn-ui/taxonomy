888eab9c6f0105c45b29ab071f3cc3b65fdb5aea
shadcn <arshad@chummun.com>
2022-11-28 12:10:11 +0400

    feat: implement og images

--- components/docs/mdx-head.tsx
+++ components/docs/mdx-head.tsx
@@ -1,12 +1,15 @@
+import * as z from "zod"
 import { allDocuments } from "contentlayer/generated"
+import { ogImageSchema } from "@/lib/validations/og"
 
 interface MdxHeadProps {
   params: {
     slug?: string[]
   }
+  og?: z.infer<typeof ogImageSchema>
 }
 
-export default function MdxHead({ params }: MdxHeadProps) {
+export default function MdxHead({ params, og }: MdxHeadProps) {
   const slug = params?.slug?.join("/") || ""
   const mdxDoc = allDocuments.find((doc) => doc.slugAsParams === slug)
 
@@ -15,6 +18,14 @@ export default function MdxHead({ params }: MdxHeadProps) {
   }
 
   const title = `${mdxDoc.title} - Taxonomy`
+  const url = process.env.NEXT_PUBLIC_APP_URL
+  let ogUrl = new URL(`${url}/og.jpg`)
+
+  if (og.type) {
+    ogUrl = new URL(url)
+    ogUrl.searchParams.set("heading", mdxDoc.title)
+    ogUrl.searchParams.set("type", og.type)
+  }
 
   return (
     <>
@@ -23,11 +34,11 @@ export default function MdxHead({ params }: MdxHeadProps) {
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width" />
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://tx.shadcn.com" />
-      <meta property="og:image" content="https://tx.shadcn.com/og.jpg" />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={ogUrl.toString()} />
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://tx.shadcn.com" />
-      <meta property="twitter:image" content="https://tx.shadcn.com/og.jpg" />
+      <meta property="twitter:url" content={url} />
+      <meta property="twitter:image" content={ogUrl.toString()} />
     </>
   )
 }
---
+++ lib/validations/og.ts
@@ -0,0 +1,6 @@
+import * as z from "zod"
+
+export const ogImageSchema = z.object({
+  heading: z.string(),
+  type: z.string(),
+})
