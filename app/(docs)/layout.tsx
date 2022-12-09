ce9c65cd14888c7297c4db15f3ca029c2b46178e
mikecebul <mikecebul@gmail.com>
2022-12-08 23:31:23 -0500

    removed unnecessary docs/layout

--- app/(docs)/layout.tsx
+++ app/(docs)/layout.tsx
@@ -39,7 +39,14 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
           </div>
         </div>
       </header>
-      <div className="container flex-1">{children}</div>
+      <div className="container flex-1">
+        <div className="flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
+          <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full flex-shrink-0 overflow-y-auto border-r border-r-slate-100 py-6 pr-2 md:sticky md:block lg:py-10">
+            <DocsSidebarNav items={docsConfig.sidebarNav} />
+          </aside>
+          {children}
+        </div>
+      </div>
       <SiteFooter />
     </div>
   )
