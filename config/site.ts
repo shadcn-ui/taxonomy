90bcb200ea4b06691fcb3c926694ccef8fd34e0e
dewedeth <yousefbacarro@icloud.com>
2023-04-23 01:05:37 -0400

    test

--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,13 @@
 import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
+  name: "dewedeth",
   description:
     "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://tx.shadcn.com",
-  ogImage: "https://tx.shadcn.com/og.jpg",
+  url: "https://dewedeth.xyz",
+  ogImage: "https://dewedeth.xyz/og.jpg",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/taxonomy",
+    twitter: "https://twitter.com/yousef_yo101",
+    github: "https://github.com/dewedeth",
   },
 }
