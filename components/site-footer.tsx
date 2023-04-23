07cba473e96e968ddacaa9dfe2a83284de7cbb41
Yousef Abdelkhaleq <yousefbacarro@icloud.com>
2023-04-23 02:42:38 -0400

    site-footer updated

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -15,7 +15,7 @@ export function SiteFooter() {
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              shadcn
+              dewedeth
             </a>
             . Hosted on{" "}
             <a
