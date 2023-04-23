5bd96bc9a2ae25d79b4cade4f3402ab89527f243
dewedeth <yousefbacarro@icloud.com>
2023-04-23 01:25:40 -0400

    test

--- config/site.ts
+++ config/site.ts
@@ -1,10 +1,10 @@
 import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "dewedeth",
+  name: "y2js",
   description:
     "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://dewedeth.xyz",
+  url: "https://y2js.xyz",
   ogImage: "https://dewedeth.xyz/og.jpg",
   links: {
     twitter: "https://twitter.com/yousef_yo101",
