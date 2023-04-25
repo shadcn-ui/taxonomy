41592a892350bf4e4ac87eb694e51dc68b4e0461
shadcn <m@shadcn.com>
2023-04-25 18:13:42 +0400

    fix: remove custom pages

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,13 +18,52 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
-  pages: {
-    signIn: "/login",
-  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || "",
       clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
+    }),
+    EmailProvider({
+      from: process.env.SMTP_FROM,
+      sendVerificationRequest: async ({ identifier, url, provider }) => {
+        const user = await db.user.findUnique({
+          where: {
+            email: identifier,
+          },
+          select: {
+            emailVerified: true,
+          },
+        })
+
+        const templateId = user?.emailVerified
+          ? process.env.POSTMARK_SIGN_IN_TEMPLATE
+          : process.env.POSTMARK_ACTIVATION_TEMPLATE
+        if (!templateId) {
+          throw new Error("Missing template id")
+        }
+
+        const result = await postmarkClient.sendEmailWithTemplate({
+          TemplateId: parseInt(templateId),
+          To: identifier,
+          From: provider.from as string,
+          TemplateModel: {
+            action_url: url,
+            product_name: siteConfig.name,
+          },
+          Headers: [
+            {
+              // Set this to prevent Gmail from threading emails.
+              // See https://stackoverflow.com/questions/23434110/force-emails-not-to-be-grouped-into-conversations/25435722.
+              Name: "X-Entity-Ref-ID",
+              Value: new Date().getTime() + "",
+            },
+          ],
+        })
+
+        if (result.ErrorCode) {
+          throw new Error(result.Message)
+        }
+      },
     }),
   ],
   callbacks: {
