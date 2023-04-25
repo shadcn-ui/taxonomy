596217f389d6f3c5848daf59508aa6e83d598683
shadcn <m@shadcn.com>
2023-04-25 18:16:37 +0400

    fix: remove middleware

--- lib/auth.ts
+++ lib/auth.ts
@@ -18,6 +18,9 @@ export const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
+  pages: {
+    signIn: "/login",
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID || "",
