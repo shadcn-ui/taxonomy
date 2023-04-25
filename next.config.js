20eb7e738ae3c83745b4cda454b02a846ec5e3da
shadcn <m@shadcn.com>
2023-04-25 19:14:02 +0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["avatars.githubusercontent.com"],
-  },
   experimental: {
     appDir: true,
   },
 }
 
-export default nextConfig
+module.exports = nextConfig
