db027315a8392a0786ac9ad78fb6f382498efd53
shadcn <m@shadcn.com>
2023-04-25 18:21:12 +0400

    fix: remove callbacks

--- lib/auth.ts
+++ lib/auth.ts
@@ -69,37 +69,37 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        session.user.id = token.id
-        session.user.name = token.name
-        session.user.email = token.email
-        session.user.image = token.picture
-      }
+  // callbacks: {
+  //   async session({ token, session }) {
+  //     if (token) {
+  //       session.user.id = token.id
+  //       session.user.name = token.name
+  //       session.user.email = token.email
+  //       session.user.image = token.picture
+  //     }
 
-      return session
-    },
-    async jwt({ token, user }) {
-      const dbUser = await db.user.findFirst({
-        where: {
-          email: token.email,
-        },
-      })
+  //     return session
+  //   },
+  //   async jwt({ token, user }) {
+  //     const dbUser = await db.user.findFirst({
+  //       where: {
+  //         email: token.email,
+  //       },
+  //     })
 
-      if (!dbUser) {
-        if (user) {
-          token.id = user?.id
-        }
-        return token
-      }
+  //     if (!dbUser) {
+  //       if (user) {
+  //         token.id = user?.id
+  //       }
+  //       return token
+  //     }
 
-      return {
-        id: dbUser.id,
-        name: dbUser.name,
-        email: dbUser.email,
-        picture: dbUser.image,
-      }
-    },
-  },
+  //     return {
+  //       id: dbUser.id,
+  //       name: dbUser.name,
+  //       email: dbUser.email,
+  //       picture: dbUser.image,
+  //     }
+  //   },
+  // },
 }
