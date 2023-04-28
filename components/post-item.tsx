e6093263e4a40b1b5a794c4082df231499de61cc
sajjad <samaralishaikh212@gmail.com>
2023-04-28 19:28:29 +0530

    update: wrap post item body inside link tag

--- components/post-item.tsx
+++ components/post-item.tsx
@@ -12,19 +12,16 @@ interface PostItemProps {
 export function PostItem({ post }: PostItemProps) {
   return (
     <div className="flex items-center justify-between p-4">
-      <div className="grid gap-1">
-        <Link
-          href={`/editor/${post.id}`}
-          className="font-semibold hover:underline"
-        >
-          {post.title}
-        </Link>
-        <div>
-          <p className="text-sm text-muted-foreground">
-            {formatDate(post.createdAt?.toDateString())}
-          </p>
+      <Link href={`/editor/${post.id}`}>
+        <div className="grid gap-1">
+          <p className="font-semibold">{post.title}</p>
+          <div>
+            <p className="text-sm text-muted-foreground">
+              {formatDate(post.createdAt?.toDateString())}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
       <PostOperations post={{ id: post.id, title: post.title }} />
     </div>
   )
