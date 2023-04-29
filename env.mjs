428b126048098603ec3e08299c2cc3e8a0f77803
shadcn <m@shadcn.com>
2023-04-29 21:03:51 +0400

    fix: make NEXTAUTH_URL optional

--- env.mjs
+++ env.mjs
@@ -3,7 +3,9 @@ import { z } from "zod"
 
 export const env = createEnv({
   server: {
-    NEXTAUTH_URL: z.string().url(),
+    // This is optional because it's only used in development.
+    // See https://next-auth.js.org/deployment.
+    NEXTAUTH_URL: z.string().url().optional(),
     NEXTAUTH_SECRET: z.string().min(1),
     GITHUB_CLIENT_ID: z.string().min(1),
     GITHUB_CLIENT_SECRET: z.string().min(1),
@@ -21,9 +23,7 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
-    NEXTAUTH_URL: process.env.VERCEL
-      ? process.env.VERCEL_URL
-      : process.env.NEXTAUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
