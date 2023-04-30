3fa527e1adc33b391df7e7dd3da37aa8ef1c1c4a
davidthomasparks <Davidthomasparks@gmail.com>
2023-04-30 16:09:39 -0700

    Robots .txt and sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,37 @@
+function generateSiteMap() {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://pixelfy.ai</loc>
+     <url>
+       <loc>https://pixelfy.ai/tos</loc>
+     </url>
+          <url>
+    <loc>https://pixelfy.ai/privacy-policy</loc>
+     </url>
+   </urlset>
+ `
+}
+
+function SiteMap() {
+    // getServerSideProps will do the heavy lifting
+}
+
+export const getServerSideProps = async (context) => {
+    const { res } = context
+
+    // We generate the XML sitemap with the posts data
+    const sitemap = generateSiteMap()
+
+    res.setHeader("Content-Type", "text/xml")
+    // we send the XML to the browser
+    res.write(sitemap)
+    res.end()
+
+    return {
+        props: {},
+    }
+}
+
+export default SiteMap
