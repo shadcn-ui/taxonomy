ab4016192a5db417012445e7f4d80aa6f516b08f
Idan <idanber1234@gmail.com>
2023-05-09 19:15:13 +0300

    adding required envs

--- env.mjs
+++ env.mjs
@@ -5,10 +5,10 @@ export const env = createEnv({
   server: {
     // This is optional because it's only used in development.
     // See https://next-auth.js.org/deployment.
-    // NEXTAUTH_URL: z.string().url().optional(),
-    // NEXTAUTH_SECRET: z.string().min(1),
-    // GOOGLE_CLIENT_ID: z.string().min(1),
-    // GOOGLE_CLIENT_SECRET: z.string().min(1),
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
+    GOOGLE_CLIENT_ID: z.string().min(1),
+    GOOGLE_CLIENT_SECRET: z.string().min(1),
     // GITHUB_CLIENT_ID: z.string().min(1),
     // GITHUB_CLIENT_SECRET: z.string().min(1),
     // GITHUB_ACCESS_TOKEN: z.string().min(1),
@@ -22,13 +22,13 @@ export const env = createEnv({
     // STRIPE_PRO_MONTHLY_PLAN_ID: z.string().min(1),
   },
   client: {
-    // NEXT_PUBLIC_APP_URL: z.string().min(1),
+    NEXT_PUBLIC_APP_URL: z.string().min(1),
   },
   runtimeEnv: {
-    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    // GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     // GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     // GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
@@ -40,6 +40,6 @@ export const env = createEnv({
     // STRIPE_API_KEY: process.env.STRIPE_API_KEY,
     // STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
     // STRIPE_PRO_MONTHLY_PLAN_ID: process.env.STRIPE_PRO_MONTHLY_PLAN_ID,
-    // NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
 })
