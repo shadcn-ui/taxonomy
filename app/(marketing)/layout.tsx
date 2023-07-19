0e26fe1a967fc1c1e3f13f422f9ed84790b645e6
krishnu9 <krishnu.binod@gmail.com>
2023-07-19 12:48:08 +0530

    Fix: #95 Login button is replaced with UserAccount after login

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link"
+import { notFound } from "next/navigation"
 
 import { marketingConfig } from "@/config/marketing"
+import { getCurrentUser } from "@/lib/session"
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
 import { MainNav } from "@/components/main-nav"
 import { SiteFooter } from "@/components/site-footer"
+import { UserAccountNav } from "@/components/user-account-nav"
 
 interface MarketingLayoutProps {
   children: React.ReactNode
@@ -13,22 +16,34 @@ interface MarketingLayoutProps {
 export default async function MarketingLayout({
   children,
 }: MarketingLayoutProps) {
+  const user = await getCurrentUser()
+
   return (
     <div className="flex min-h-screen flex-col">
       <header className="container z-40 bg-background">
         <div className="flex h-20 items-center justify-between py-6">
           <MainNav items={marketingConfig.mainNav} />
-          <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
-            >
-              Login
-            </Link>
-          </nav>
+          {user ? (
+            <UserAccountNav
+              user={{
+                name: user.name,
+                email: user.email,
+                image: user.image,
+              }}
+            />
+          ) : (
+            <nav>
+              <Link
+                href="/login"
+                className={cn(
+                  buttonVariants({ variant: "secondary", size: "sm" }),
+                  "px-4"
+                )}
+              >
+                Login
+              </Link>
+            </nav>
+          )}
         </div>
       </header>
       <main className="flex-1">{children}</main>
