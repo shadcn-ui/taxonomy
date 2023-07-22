a5a6bf32a51a937e153db90b846738257699c66f
Dikshant <khuranadikshant3@gmail.com>
2023-07-22 12:30:26 +0530

    FIX #181 alligned the signup form at center

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RegisterPage() {
   return (
-    <div className="container relative grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:px-0">
       <Link
         href="/login"
         className={cn(
@@ -22,7 +22,7 @@ export default function RegisterPage() {
       >
         Login
       </Link>
-      <div className="lg:p-8 absolute top-1/2	left-1/2	transform -translate-x-1/2 -translate-y-1/2">
+      <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
             <Icons.logo className="mx-auto h-6 w-6" />
