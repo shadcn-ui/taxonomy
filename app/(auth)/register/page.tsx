d36f4fb447e9d104376cd4acd3bee2e5ca108663
Rohit Sain <rsainwork@gmail.com>
2023-08-02 15:20:39 +0530

    centering the signup form removing the left hidden div

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 
 export default function RegisterPage() {
   return (
-    <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container grid h-screen w-screen flex-col items-center justify-center lg:max-w-none lg:px-0"> {/* removing the lg:grid-cols-2 to not divide the screen space */}
       <Link
         href="/login"
         className={cn(
@@ -22,7 +22,7 @@ export default function RegisterPage() {
       >
         Login
       </Link>
-      <div className="hidden h-full bg-muted lg:block" />
+      {/* <div className="hidden h-full w-1/2 lg:block" /> */}  {/* removing the left div to make the whole screen for the form on the right */}
       <div className="lg:p-8">
         <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
           <div className="flex flex-col space-y-2 text-center">
