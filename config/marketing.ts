b050e01335b978c7b8157a8752ade6d1d694a1af
Ehtisham Sajjad <47937929+ehtishamsajjad1@users.noreply.github.com>
2023-08-23 02:41:20 +0500

    Refactor: Remove '#' from slug for consistency in features

--- config/marketing.ts
+++ config/marketing.ts
@@ -4,7 +4,7 @@ export const marketingConfig: MarketingConfig = {
   mainNav: [
     {
       title: "Features",
-      href: "/#features",
+      href: "/features",
     },
     {
       title: "Pricing",
