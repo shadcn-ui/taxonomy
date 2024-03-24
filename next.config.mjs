344471d0791b48ead6731c2617c62cace481747d
zce <w@zce.me>
2024-03-24 20:24:15 +0800

    feat: velite plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 import "./env.mjs"
-// import { build } from "velite"
+import { build } from "velite"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,23 +7,21 @@ const nextConfig = {
     domains: ["avatars.githubusercontent.com"],
   },
   webpack: config => {
-    // config.plugins.push(new VeliteWebpackPlugin())
+    config.plugins.push(new VeliteWebpackPlugin())
     return config
   }
 }
 
-// class VeliteWebpackPlugin {
-//   static started = false
-//   apply(/** @type {import('webpack').Compiler} */ compiler) {
-//     // executed three times in nextjs
-//     // twice for the server (nodejs / edge runtime) and once for the client
-//     compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
-//       if (VeliteWebpackPlugin.started) return
-//       VeliteWebpackPlugin.started = true
-//       const dev = compiler.options.mode === 'development'
-//       await build({ watch: dev, clean: !dev })
-//     })
-//   }
-// }
+class VeliteWebpackPlugin {
+  static started = false
+  apply(compiler) {
+    compiler.hooks.beforeCompile.tapPromise('VeliteWebpackPlugin', async () => {
+      if (VeliteWebpackPlugin.started) return
+      VeliteWebpackPlugin.started = true
+      const dev = compiler.options.mode === 'development'
+      await build({ watch: dev, clean: !dev })
+    })
+  }
+}
 
 export default nextConfig
